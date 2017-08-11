f29ddfebd20c87dc1c38254ac2ae0c2ed7950a4e
Lukaviy <jiukaviy@gmail.com>
2017-08-11 17:31:05 +0900

    Update random-agent

--- random_agent.cpp
+++ random_agent.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-
+#include <fstream>
+#include <time.h>
 using namespace std;
 
 int main() {
-	int x_size, y_size, area_size;
+	int x_size, y_size, area_size, players_count;
+	char letter;
 
-	cin >> x_size >> y_size >> area_size;
+	cin >> x_size >> y_size >> area_size >> players_count >> letter;
 
 	char moves[] = { 'L', 'R', 'U', 'D', 'S' };
 
+	srand(time(0));
+
 	for (;;) {
+		//int step;
+		//cin >> step;
+		for (int y = 0; y < y_size; y++) {
+			for (int x = 0; x < x_size; x++) {
+				char t;
+				cin >> t;
+			}
+		}
+
 		cout << moves[rand() % sizeof moves] << endl;
-		cout.flush();
 	}
 }
